b8c2458e229f33161b93b7335aa1a6a6b0e3f617
bhrthkshr <aninjahattori@gmail.com>
2019-12-21 18:12:22 +0530

    404

--- components/ui/contact-us/contactus.tsx
+++ components/ui/contact-us/contactus.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Box, Typography } from '@material-ui/core'
+
 
 export default function Contactus() {
     return (
         <div>
-            conttttttttttt
+               <Box display="flex" height="100vh" flexDirection="column" justifyContent="center" alignItems="center">
+              <Box flexDirection="row" display="flex" width="100%" height="300px" justifyContent="center">
+              <img src="assets/img/404.jpg" style={{maxWidth:"100%",height:"auto",textAlign:"center"}} alt=""/>
+              </Box>
+                <Typography variant="h3" component="div">
+                  <Box textAlign="center" p={1}>
+                 We are working on building this page
+                  </Box>
+                  </Typography>
+            </Box>
         </div>
     )
 }
